842e37e7dd12c4b246b704ddbc34cc464daef49d
Verena Tanzil <verena.tanzil@live.com>
2018-10-27 00:07:55 -0400

    Almost Done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,53 +6,54 @@
 // question dissapear
 // result came up, correct, incorrect and unanswered
 
-
-// array for the questions
-
+// variables for score and timer
 var quizArea = $('#quest');
-
-$(document).on('click', '#start', function(e) {
-    begin();
-});
-  
-  $(document).on('click', '#submit', function(e) {
-    finish();
-});
-
-var questions = [{
-    q: "What is the smallest planet in the Solar System?",
-    a: ["Mercury","Jupiter","Mars","Uranus"],
-    c: "Mercury",
-},{
-    q: "What is the largest planet in the Solar System?",
-    a: ["Earth","Saturn","Jupiter","Mars"],
-    c: "Jupiter",
-},{
-    q: "What planet is nicknamed the ‘Red Planet’?",
-    a: ["Mars","Jupiter","Venus","Neptune"],
-    c: "Mars",
-},{
-    q: "What is the brightest planet in the night sky?",
-    a: ["Neptune","Venus","Mars","Saturn"],
-    c: "Venus",
-},{
-    q: "What planet is closest to the Sun?",
-    a: ["Mercury","Jupiter","Mars","Uranus"],
-    c: "Mercury",
-},{
-    q: "What planet is closest in size to Earth?",
-    a: ["Mars","Jupiter","Venus","Neptune"],
-    c: "Venus",
-},{
-    q: "What planet in the solar system is farthest from the Sun?",
-    a: ["Neptune","Venus","Mars","Saturn"],
-    c: "Neptune",
-}]
-
 var correct = 0;
 var incorrect = 0;
 var counter = 70;
 
+// click function
+// start button
+$(document).on('click', '#start', function(e) {
+    begin();
+});
+//  submit button 
+  $(document).on('click', '#submit', function(e) {
+    finish();
+});
+
+// array for the questions
+var questions = [{
+    q: "What is the smallest planet in the Solar System?",
+    a: [" Mercury "," Jupiter "," Mars "," Uranus "],
+    c: " Mercury ",
+},{
+    q: "What is the largest planet in the Solar System?",
+    a: [" Earth "," Saturn "," Jupiter "," Mars "],
+    c: " Jupiter ",
+},{
+    q: "What planet is nicknamed the ‘Red Planet’?",
+    a: [" Mars "," Jupiter "," Venus "," Neptune "],
+    c: " Mars ",
+},{
+    q: "What is the brightest planet in the night sky?",
+    a: [" Neptune "," Venus "," Mars "," Saturn "],
+    c: " Venus ",
+},{
+    q: "What planet is closest to the Sun?",
+    a: [" Mercury "," Jupiter "," Mars "," Uranus "],
+    c: " Mercury ",
+},{
+    q: "What planet is closest in size to Earth?",
+    a: [" Mars "," Jupiter "," Venus "," Neptune "],
+    c: " Venus ",
+},{
+    q: "What planet in the solar system is farthest from the Sun?",
+    a: [" Neptune "," Venus "," Mars "," Saturn "],
+    c: " Neptune ",
+}]
+
+// countdown function
 function countdown(){
     counter--;
     $('#counternum').html(counter);
@@ -62,6 +63,7 @@ function countdown(){
     }
 }
 
+// when start button is clicked, timer appear, question popped uout and submit button appear
 function begin() {
     timer = setInterval(countdown, 1000) 
     console.log("test")
@@ -72,65 +74,76 @@ function begin() {
     for (var i = 0; i < questions.length; i++) {
         quizArea.append('<h2>' + questions[i].q + '</h2>');
         for (var j = 0; j < questions[i].a.length; j++) {
-          quizArea.append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].a[j] + '">' + questions[i].a[j]);
+          quizArea.append('<input type="radio" name="q' + '-' + i + '" value="' + questions[i].a[j] + '">' + questions[i].a[j]);
         }
       }  quizArea.append('<button id="submit">Submit</button>');
 };
-    
+
+// when submit button is clicked, each question is being checked to see if the answer match the question, 
+// if they match, correct increase by one 
 function finish() {
-    console.log("test");
-
-   
-
-        $.each($("input[name='q-0']:checked"), function() {
-            if ($(this).val() == questions[0].c) {
-            correct++;
-            } else {
-            incorrect++;
-            }
-        });
-        $.each($("input[name='q-1']:checked"), function() {
-            if ($(this).val() == questions[1].c) {
-            correct++;
-            } else {
-            incorrect++;
-            }
-        });
-        $.each($("input[name='q-2']:checked"), function() {
-            if ($(this).val() == questions[2].c) {
-            correct++;
-            } else {
-            incorrect++;
-            }
-        });
-        $.each($("input[name='q-3']:checked"), function() {
-            if ($(this).val() == questions[3].c) {
-            correct++;
-            } else {
-            incorrect++;
-            }
-        });
-        $.each($("input[name='q-4']:checked"), function() {
-            if ($(this).val() == questions[4].c) {
-            correct++;
-            } else {
-            incorrect++;
-            }
-        });
-        $.each($("input[name='q-5']:checked"), function() {
-            if ($(this).val() == questions[5].c) {
-            correct++;
-            } else {
-            ncorrect++;
-            }
-        });
-        $.each($("input[name='q-6']:checked"), function() {
-            if ($(this).val() == questions[6].c) {
-            correct++;
-            } else {
-            incorrect++;
-            }
-        });
+    $.each($("input[name='q-0']:checked"), function() {
+        if ($(this).val() == questions[0].c) {
+        correct++;
+        } else {
+        incorrect++;
+        }
+    });
+    $.each($("input[name='q-1']:checked"), function() {
+        if ($(this).val() == questions[1].c) {
+        correct++;
+        } else {
+        incorrect++;
+        }
+    });
+    $.each($("input[name='q-2']:checked"), function() {
+        if ($(this).val() == questions[2].c) {
+        correct++;
+        } else {
+        incorrect++;
+        }
+    });
+    $.each($("input[name='q-3']:checked"), function() {
+        if ($(this).val() == questions[3].c) {
+        correct++;
+        } else {
+        incorrect++;
+        }
+    });
+    $.each($("input[name='q-4']:checked"), function() {
+        if ($(this).val() == questions[4].c) {
+        correct++;
+        } else {
+        incorrect++;
+        }
+    });
+    $.each($("input[name='q-5']:checked"), function() {
+        if ($(this).val() == questions[5].c) {
+        correct++;
+        } else {
+        ncorrect++;
+        }
+    });
+    $.each($("input[name='q-6']:checked"), function() {
+        if ($(this).val() == questions[6].c) {
+        correct++;
+        console.log("correct");
+        } else {
+        incorrect++;
+        }
+    });
+    result();
+    
+        
 };
 
-
+// when submit button is clicked and the time run out, the result shows up
+// the timer is remove and the question dissapear
+function result(){
+    clearInterval(timer);
+    $('#subWrapper h2').remove();
+    quizArea.html('<h2 class="gj">Great Job!</h2>');
+    quizArea.append('<h3>Correct Answers: ' + correct + '</h3>');
+    quizArea.append('<h3>Incorrect Answers: ' + incorrect + '</h3>');
+    quizArea.append('<h3>Unanswered Questions: ' + (questions.length - (incorrect + correct)) + '</h3>');
+}
